bfa3f3e96c6e0c87fceafb3e24c158e34372307c
Abrar Ahmed <99709358+AbrarAhmed111@users.noreply.github.com>
2024-09-11 23:49:54 +0500

    updated UI

--- src/components/marketplace/index.js
+++ src/components/marketplace/index.js
@@ -4,6 +4,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/mousewheel";
 import Web3 from "web3";
+import { FaPlus } from "react-icons/fa6";
+import { FaThList } from "react-icons/fa";
 import {
     rabbitNFTAbi,
     rabbitNFTAddress,
@@ -340,8 +342,8 @@ export default function Team() {
                     providing higher rewards.
                 </p>
                 <div className="flex  w-full flex-wrap items-center justify-center px-2 py-6 bg-[#e647bf]/10 rounded-lg gap-x-5 gap-y-16 flex-row">
-                    {loading ? (
-                        <div className="w-full flex justify-center mt-24 mb-24">
+                    {false ? (
+                        <div className="flex justify-center w-full mt-24 mb-24">
                             <div role="status">
                                 <svg
                                     aria-hidden="true"
@@ -373,14 +375,23 @@ export default function Team() {
                                             <img
                                                 src={data.image}
                                                 alt=""
-                                                className="object-cover transition-transform duration-300 transform sm:h-[280px] sm:w-[220px] md:h-[300px] lg:w-[260px] lg:h-[320px] w-[300px] h-[300px] md:w-[240px] group-hover:scale-125 "
+                                                className="object-cover transition-transform duration-300 transform sm:h-[280px] sm:w-[250px] md:h-[300px] lg:w-[300px] lg:h-[320px] w-[300px] h-[300px] md:w-[280px] group-hover:scale-125 "
                                             />
                                             {/* Mint ID box */}
-                                            <div className="absolute top-2 right-2 bg-white border border-gray-300 rounded-lg px-2 py-1 text-md text-gray-800 shadow-md">
-                                                Id: {data.mintId}
+                                        
+                                            <div className="absolute px-2 py-1 font-bold text-[#44134e]/90 bg-white border border-gray-300 rounded-lg shadow-md top-2 right-2 text-md">
+                                                <span className="text-sm">ID:</span> <span className="font-mono text-lg">{data.mintId}</span>
                                             </div>
-                                            <button
-                                                className="relative z-10 px-4 cursor-pointer font-semibold w-full text-center py-2 bg-[#d459b6] hover:bg-[#e647bf] text-white rounded-lg"
+                                            <div
+                                                className="relative z-10 px-2  py-2 bg-[#c04ea4] flex w-full items-center justify-between  text-white rounded-lg"
+                                            >
+                                                <div>
+                                                    <h6 className="text-[#fcfcfc] font-semibold mb-1">Type</h6>
+                                                    <h6 className="font-mono text-sm font-bold text-[#44134e]/90 ">950.2</h6>
+                                                </div>
+
+                                                <button
+                                                className="relative z-10 px-4 cursor-pointer font-semibold w-[150px] shadow-xl text-center py-2 bg-[#d459b6] hover:bg-[#e647bf] text-white rounded-lg"
                                                 onClick={() =>
                                                     handleListNft(data)
                                                 }
@@ -423,19 +434,65 @@ export default function Team() {
                                                     </>
                                                 )}
                                             </button>
+                                               
+                                            </div>
+                                            {/* <button
+                                                className="relative z-10 px-4 cursor-pointer font-semibold w-full text-center py-2 bg-[#d459b6] hover:bg-[#e647bf] text-white rounded-lg"
+                                                onClick={() =>
+                                                    handleListNft(data)
+                                                }
+                                                disabled={
+                                                    loadingMintId &&
+                                                    loadingMintId !==
+                                                        data.mintId
+                                                }
+                                            >
+                                                {loadingMintId ===
+                                                data.mintId ? (
+                                                    <>
+                                                        <svg
+                                                            aria-hidden="true"
+                                                            role="status"
+                                                            className="inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600"
+                                                            viewBox="0 0 100 101"
+                                                            fill="none"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                        >
+                                                            <path
+                                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                                fill="currentColor"
+                                                            />
+                                                            <path
+                                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                                fill="#1C64F2"
+                                                            />
+                                                        </svg>
+                                                        Loading...
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        {data.isListed
+                                                            ? walletAddress ===
+                                                              data.seller
+                                                                ? "Delist "
+                                                                : " Buy NFT"
+                                                            : "Buy"}
+                                                    </>
+                                                )}
+                                            </button> */}
                                         </div>
                                     </div>
                                 ))
                             ) : (
-                                <div className="w-full flex justify-center mt-24 mb-24">
+                                <div className="flex justify-center w-full mt-24 mb-24">
                                     <div className="text-[24px] text-white/70 font-bold">
                                         No NFT Found
                                     </div>
                                 </div>
                             )}
                             {currentNFTs.length > 0 && (
-                                  <div className="w-full flex justify-center">
-                                  <div className="w-full md:w-1/5 flex justify-between items-center mt-6">
+                                  <div className="flex justify-center w-full">
+                                  <div className="flex items-center justify-between w-full mt-6 md:w-1/5">
                                       <button
                                           className="px-2 py-1 md:px-4 md:py-2 bg-[#d459b6] hover:bg-[#e647bf] text-white rounded-lg text-sm md:text-base"
                                           onClick={previousPage}
@@ -443,7 +500,7 @@ export default function Team() {
                                       >
                                           Previous
                                       </button>
-                                      <span className="text-white text-sm md:text-base">
+                                      <span className="text-sm text-white md:text-base">
                                           Page {currentPage} of {totalPages}
                                       </span>
                                       <button
